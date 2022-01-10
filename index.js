ddb25923f43c82fc5d383320f4b6173920a73e97
Min Khant Kyaw <minkhantkyaw.99.phoethar@gmail.com>
2022-01-10 23:08:33 +0630

    Basic Buttons coded

---
+++ index.js
@@ -0,0 +1,63 @@
+const calcData = [
+    { id: "clear", value: "AC"},
+    { id: "divde", value: "/"},
+    { id: "multiply", value: "x"},
+    { id: "seven", value: 7},
+    { id: "eight", value: 8},
+    { id: "nine", value: 9},
+    { id: "subtract", value: "-"},
+    { id: "four", value: 4},
+    { id: "five", value: 5},
+    { id: "six", value: 6},
+    { id: "add", value: "+"},
+    { id: "one", value: 1},
+    { id: "two", value: 2},
+    { id: "three", value: 3},
+    { id: "equals", value: "="},
+    { id: "zero", value: 0},
+    { id: "decimal", value: "."},
+]
+
+const operators = ["AC", "/", "x", "+", "-", "="];
+
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+const Display = ({ input, output }) => (
+    <div className="output">
+        <span className="result">{output}</span>
+        <span id="display" className="input">{input}</span>
+    </div>
+);
+
+const Key = ({ keyData : {id, value}, handleInput}) => (
+    <button id={id} onClick={() => handleInput(value)}>
+        {value}
+    </button>
+);
+
+const Keyboard = ({handleInput}) => (
+    <div className="keys">
+        {calcData.map((key) => (
+            <Key key={key.id} keyData={key} handleInput={handleInput} />
+        ))}
+    </div>
+);
+
+
+const App = () => {
+    const [input, setInput] = React.useState("0");
+    const [output, setOutput] = React.useState("");
+
+    const handleInput = () => {}
+
+    return (
+        <div className="container">
+            <div className="calculator">
+                <Display input={input} output={output} />
+                <Keyboard handleInput={handleInput}/>
+            </div>
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
